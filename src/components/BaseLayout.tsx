36188cd8cadd4622f6914e70f086033f8b20d9f3
raka-raprast <raprast.raka@gmail.com>
2024-01-20 12:16:46 +0800

    add global loading state

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode, useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
+import Head from "next/head";
+import ReactLoading from "react-loading";
 
 interface BaseLayoutProps {
   children: ReactNode;
@@ -7,15 +9,29 @@ interface BaseLayoutProps {
 
 const BaseLayout: React.FC<BaseLayoutProps> = ({ children }) => {
   const [isMobile, setIsMobile] = useState<boolean | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       const mobileStatus = window.innerWidth <= 600;
       setIsMobile(mobileStatus);
+      setIsLoading(false);
     }
   }, []);
+  if (isLoading) {
+    return (
+      <div className="loadingBarFullScreen">
+        <ReactLoading type="bars" color="#7fc7d9" height={450} width={375} />
+      </div>
+    );
+  }
   return (
     <div className="layout">
+      <Head>
+        <title>Raprast</title>
+        <link rel="icon" href="/logo.ico" />
+        <link rel="icon" type="image/png" href="/ai_avatar.png" />
+      </Head>
       <Sidebar />
       {isMobile ? (
         <main className="layout__main-content_mobile">{children}</main>
