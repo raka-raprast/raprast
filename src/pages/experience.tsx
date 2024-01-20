637f8e379dbc09b91ca5a3dfcd4ff096a773ddc2
raka-raprast <raprast.raka@gmail.com>
2024-01-20 10:57:36 +0800

    add isLoading

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -4,8 +4,11 @@ import WorkCard from "@/components/WorkCard";
 import WorkExperienceData from "@/data/workExperience.json";
 import { EducationExperience, WorkExperience } from "@/models/experience_model";
 import EducationCard from "@/components/EducationCard";
+import ReactLoading from "react-loading";
 
 const Experience: React.FC = () => {
+  const [isWorkLoading, setIsWorkLoading] = useState(true);
+  const [isEducationLoading, setIsEducationLoading] = useState(true);
   const [workExperienceData, setWorkExperienceData] = useState<
     WorkExperience[]
   >([]);
@@ -16,69 +19,84 @@ const Experience: React.FC = () => {
   useEffect(() => {
     import("@/data/workExperience.json").then((data) => {
       setWorkExperienceData(data.workExperience);
+      setIsWorkLoading(false);
     });
   }, []);
   useEffect(() => {
     import("@/data/educationExperience.json").then((data) => {
       setEducationExperienceData(data.educationExperience);
+      setIsEducationLoading(false);
     });
   }, []);
 
   return (
     <BaseLayout>
       <div className="home">
-        <h2
-          style={
-            {
-              marginBottom: "20px",
-              paddingBottom: "5px",
-              borderBottom: "2px solid rgb(208, 208, 208)",
-            } as React.CSSProperties
-          }
-        >
-          Professional Experience
-        </h2>
-        <div
-          style={
-            {
-              display: "grid",
-              gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-              columnGap: "45px",
-            } as React.CSSProperties
-          }
-        >
-          {workExperienceData.map((experience, index) => (
-            <div key={index}>
-              <WorkCard workExperience={experience} />
+        {isEducationLoading && isWorkLoading ? (
+          <div className="loadingBar">
+            <ReactLoading
+              type="bars"
+              color="#7fc7d9"
+              height={450}
+              width={375}
+            />
+          </div>
+        ) : (
+          <div>
+            <h2
+              style={
+                {
+                  marginBottom: "20px",
+                  paddingBottom: "5px",
+                  borderBottom: "2px solid rgb(208, 208, 208)",
+                } as React.CSSProperties
+              }
+            >
+              Professional Experience
+            </h2>
+            <div
+              style={
+                {
+                  display: "grid",
+                  gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+                  columnGap: "45px",
+                } as React.CSSProperties
+              }
+            >
+              {workExperienceData.map((experience, index) => (
+                <div key={index}>
+                  <WorkCard workExperience={experience} />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <h2
-          style={
-            {
-              marginBottom: "20px",
-              paddingBottom: "5px",
-              borderBottom: "2px solid rgb(208, 208, 208)",
-            } as React.CSSProperties
-          }
-        >
-          Educational Experience
-        </h2>
-        <div
-          style={
-            {
-              display: "grid",
-              gridTemplateColumns: "repeat(auto-fit, minmax(325px, 1fr))",
-              columnGap: "45px",
-            } as React.CSSProperties
-          }
-        >
-          {educationExperienceData.map((experience, index) => (
-            <div key={index}>
-              <EducationCard educationExperience={experience} />
+            <h2
+              style={
+                {
+                  marginBottom: "20px",
+                  paddingBottom: "5px",
+                  borderBottom: "2px solid rgb(208, 208, 208)",
+                } as React.CSSProperties
+              }
+            >
+              Educational Experience
+            </h2>
+            <div
+              style={
+                {
+                  display: "grid",
+                  gridTemplateColumns: "repeat(auto-fit, minmax(325px, 1fr))",
+                  columnGap: "45px",
+                } as React.CSSProperties
+              }
+            >
+              {educationExperienceData.map((experience, index) => (
+                <div key={index}>
+                  <EducationCard educationExperience={experience} />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
+        )}
       </div>
 
       <div className="footer">© 2024 Raka Ramadhani Aulia Prasetyo</div>
--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -2,59 +2,78 @@ import React, { useEffect, useState } from "react";
 import BaseLayout from "@/components/BaseLayout";
 import { Project } from "@/models/projects_model";
 import ProjectCard from "@/components/ProjectCard";
+import ReactLoading from "react-loading";
 
 const Project: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [projectData, setProjectData] = useState<Project[]>([]);
 
   useEffect(() => {
     import("@/data/projects.json").then((data) => {
       setProjectData(data.projects);
+      setIsLoading(false);
     });
   }, []);
   return (
     <BaseLayout>
       <div className="home">
-        <h2
-          style={
-            {
-              marginBottom: "20px",
-              paddingBottom: "5px",
-              borderBottom: "2px solid rgb(208, 208, 208)",
-            } as React.CSSProperties
-          }
-        >
-          Projects
-        </h2>
-        <p
-          style={
-            { color: "#545454", textAlign: "justify" } as React.CSSProperties
-          }
-        >
-          Dive into the narrative of my past projects, where creativity and
-          expertise collided to form a symphony of innovation. From concept to
-          completion, each project tells a story of strategic planning,
-          collaborative teamwork, and overcoming challenges. Explore the
-          milestones achieved, lessons learned, and the tangible impact on the
-          world around us. These projects are not just a testament to my skills
-          but a vibrant canvas reflecting my passion for pushing boundaries and
-          crafting solutions that stand the test of time.
-        </p>
-        <div
-          style={
-            {
-              paddingTop: "30px",
-              display: "grid",
-              gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
-              columnGap: "15px",
-            } as React.CSSProperties
-          }
-        >
-          {projectData.map((project, index) => (
-            <div key={index}>
-              <ProjectCard project={project} />
+        {isLoading ? (
+          <div className="loadingBar">
+            <ReactLoading
+              type="bars"
+              color="#7fc7d9"
+              height={450}
+              width={375}
+            />
+          </div>
+        ) : (
+          <div>
+            <h2
+              style={
+                {
+                  marginBottom: "20px",
+                  paddingBottom: "5px",
+                  borderBottom: "2px solid rgb(208, 208, 208)",
+                } as React.CSSProperties
+              }
+            >
+              Projects
+            </h2>
+            <p
+              style={
+                {
+                  color: "#545454",
+                  textAlign: "justify",
+                } as React.CSSProperties
+              }
+            >
+              Dive into the narrative of my past projects, where creativity and
+              expertise collided to form a symphony of innovation. From concept
+              to completion, each project tells a story of strategic planning,
+              collaborative teamwork, and overcoming challenges. Explore the
+              milestones achieved, lessons learned, and the tangible impact on
+              the world around us. These projects are not just a testament to my
+              skills but a vibrant canvas reflecting my passion for pushing
+              boundaries and crafting solutions that stand the test of time.
+            </p>
+            <div
+              style={
+                {
+                  paddingTop: "30px",
+                  display: "grid",
+                  gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+                  columnGap: "15px",
+                } as React.CSSProperties
+              }
+            >
+              {projectData.map((project, index) => (
+                <div key={index}>
+                  <ProjectCard project={project} />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
+        )}
       </div>
       <div className="footer">© 2024 Raka Ramadhani Aulia Prasetyo</div>
     </BaseLayout>
