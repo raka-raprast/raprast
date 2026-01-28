d2b0ed0dc089da3dc3fb89dc1d34fc941372ca35
raka-raprast <raprast.raka@gmail.com>
2026-01-29 00:06:19 +0700

    adjust font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'mbilan': ['Mbilan', 'sans-serif'],
+        'flipboard': ['Flipboard Sans', 'sans-serif'],
+      },
     },
   },
   plugins: [],
